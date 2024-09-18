a14cc9a6a27ee0699c7609490771bde9a1b960b5
Pratik Gondkar <131374322+pratikgondkar@users.noreply.github.com>
2024-09-18 22:58:29 +0530

    Create variable.tf

---
+++ Task-02/SG/variable.tf
@@ -0,0 +1,13 @@
+variable "aws_SG_public" {
+  type = string
+  default = "public-SG"
+}
+
+variable "aws_SG_private" {
+  type = string
+  default = "private-SG"
+}
+
+variable "aws_vpc" {
+  type = string
+}
