1ed5b7bed6e8bb5e19c34011ada413b3bb343629
Pratik Gondkar <131374322+pratikgondkar@users.noreply.github.com>
2024-09-18 22:57:55 +0530

    Create main.tf

---
+++ Task-02/SG/main.tf
@@ -0,0 +1,62 @@
+# security group for public
+resource "aws_security_group" "SG-01" {
+  vpc_id      = var.aws_vpc
+  ingress  {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = var.aws_SG_public
+  }
+}
+# security group for private
+resource "aws_security_group" "SG-02" {
+  vpc_id      = var.aws_vpc
+  ingress  {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress  {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress  {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = var.aws_SG_private
+  }
+}
